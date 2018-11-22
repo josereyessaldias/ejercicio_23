1c70bc659db8bb7af1f810e3cef8d7b0814b7f5c
josereyessaldias <jpreyess@uc.cl>
2018-11-22 03:45:26 -0300

    listo al fin

---
+++ config/initializers/assets.rb
@@ -0,0 +1,30 @@
+# Be sure to restart your server when you modify this file.
+
+# Version of your assets, change this if you want to expire all your assets.
+Rails.application.config.assets.version = '1.0'
+
+# Add additional assets to the asset load path.
+# Rails.application.config.assets.paths << Emoji.images_path
+# Add Yarn node_modules folder to the asset load path.
+Rails.application.config.assets.paths << Rails.root.join('node_modules')
+Rails.application.config.assets.paths << Rails.root.join('vendor','css')
+Rails.application.config.assets.paths << Rails.root.join('vendor','bfonts')
+Rails.application.config.assets.paths << Rails.root.join('vendor','afonts')
+Rails.application.config.assets.paths << Rails.root.join('vendor','cfonts')
+Rails.application.config.assets.paths << Rails.root.join('vendor','ico')
+Rails.application.config.assets.paths << Rails.root.join('vendor','images')
+Rails.application.config.assets.paths << Rails.root.join('vendor','js')
+Rails.application.config.assets.paths << Rails.root.join('vendor')
+
+# Precompile additional assets.
+# application.js, application.css, and all non-JS/CSS in the app/assets
+# folder are already added.
+# Rails.application.config.assets.precompile += %w( admin.js admin.css )
+Rails.application.config.assets.precompile += %w( *.png )
+Rails.application.config.assets.precompile += %w( *.jpg )
+Rails.application.config.assets.precompile += %w( *.gif )
+Rails.application.config.assets.precompile += %w( *.php )
+Rails.application.config.assets.precompile += %w( *.svg )
+Rails.application.config.assets.precompile += %w( *.ttf )
+Rails.application.config.assets.precompile += %w( *.eot )
+Rails.application.config.assets.precompile += %w( *.woff )
